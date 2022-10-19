3f820459a29c561fcd2e3693230dd404831def52
li-bing-zhen <951983940@qq.com>
2022-10-20 00:13:39 +0800

    雪花ID生成工具类SnowflakeIdWorker

---
+++ common-utils/src/main/java/com/flrjcx/xypt/common/config/IdWorkerConfiguration.java
@@ -0,0 +1,74 @@
+package com.flrjcx.xypt.common.config;
+
+import com.flrjcx.xypt.common.utils.SnowflakeIdWorker;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
+/**
+ * 雪花ID配置
+ */
+@Configuration
+public class IdWorkerConfiguration {
+
+    @Value("${id.work:noWorkId}")
+    private String workId;
+    @Value("${id.dateSource:noDateSource}")
+    private String dateSource;
+
+    @Bean
+    @Primary
+    public SnowflakeIdWorker idWorker() {
+        return new SnowflakeIdWorker(getWorkFromConfig(), getDateFromConfig());
+    }
+
+    private Long getWorkFromConfig() {
+        if ("noWorkId".equals(workId)) {
+            return getWorkId();
+        } else {
+            //将workId转换为Long
+            return 2L;
+        }
+    }
+
+    private Long getDateFromConfig() {
+        if ("noDateSource".equals(dateSource)) {
+            return getDataCenterId();
+        } else {
+            //将workId转换为Long
+            return 2L;
+        }
+    }
+
+    private Long getWorkId() {
+        try {
+            String hostAddress = Inet4Address.getLocalHost().getHostAddress();
+            int[] ints = StringUtils.toCodePoints(hostAddress);
+            int sums = 0;
+            for (int b : ints) {
+                sums += b;
+            }
+            return (long) (sums % 32);
+        } catch (UnknownHostException e) {
+            // 如果获取失败，则使用随机数备用
+            return RandomUtils.nextLong(0, 31);
+        }
+    }
+
+    private Long getDataCenterId() {
+        int[] ints = StringUtils.toCodePoints(SystemUtils.getHostName());
+        int sums = 0;
+        for (int i : ints) {
+            sums += i;
+        }
+        return (long) (sums % 32);
+    }
+
+}
