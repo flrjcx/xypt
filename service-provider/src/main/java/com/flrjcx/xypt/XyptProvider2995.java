cf1da6c0693418570284676f854e566db52fe05e
flrjcx <flrjcx@163.com>
2022-10-21 18:05:11 +0800

    新增nacos

--- service-provider/src/main/java/com/flrjcx/xypt/XyptProvider2995.java
+++ service-provider/src/main/java/com/flrjcx/xypt/XyptProvider2995.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -19,6 +20,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan(basePackages = { "com.flrjcx.xypt.*" })
 @MapperScan(basePackages = "com.flrjcx.xypt.mapper")
+@EnableDiscoveryClient
 public class XyptProvider2995 implements CommandLineRunner {
     public static void main(String[] args){
         SpringApplication.run(XyptProvider2995.class, args);
