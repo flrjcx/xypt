304e2b614019d3fc32b10810584fe2701158cebd
aftermath <3368760963@qq.com>
2022-10-26 14:29:30 +0800

    整理pom

--- backend-service-provider/src/main/java/com/flrjcx/xypt/XyptService2996.java
+++ backend-service-provider/src/main/java/com/flrjcx/xypt/XyptService2996.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -20,6 +22,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan(basePackages = { "com.flrjcx.xypt.*" })
 @MapperScan(basePackages = "com.flrjcx.xypt.mapper")
+@EnableFeignClients(basePackages = "com.flrjcx.xypt.client",defaultConfiguration = FeignClientProperties.FeignClientConfiguration.class)
 @EnableDiscoveryClient
 public class XyptService2996 implements CommandLineRunner {
     public static void main(String[] args){
