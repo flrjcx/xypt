91a92a01e09502f4493ee99f9fe7a3850529ff5f
flrjcx <flrjcx@163.com>
2022-11-21 09:27:19 +0800

    校验token

--- common-interface/src/main/java/com/flrjcx/xypt/controller/CheckController.java
+++ common-interface/src/main/java/com/flrjcx/xypt/controller/CheckController.java
@@ -3,15 +3,21 @@ package com.flrjcx.xypt.controller;
 import cn.hutool.dfa.FoundWord;
 import cn.hutool.dfa.SensitiveUtil;
 import com.flrjcx.xypt.common.annotation.ApiRestController;
+import com.flrjcx.xypt.common.annotation.Validation;
 import com.flrjcx.xypt.common.enums.ResultCodeEnum;
+import com.flrjcx.xypt.common.handler.ValidationInterceptor;
 import com.flrjcx.xypt.common.model.result.ResponseData;
 import com.flrjcx.xypt.common.utils.SensitiveWordUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,6 +29,10 @@ import java.util.Objects;
 @ApiRestController("/api/common/verify")
 @Log4j2
 public class CheckController {
+
+    @Resource
+    private ValidationInterceptor validationInterceptor;
+
     @ApiOperation("文本检查")
     @PostMapping("/text/check")
     public ResponseData textCheck(@RequestBody String text) {
@@ -39,4 +49,12 @@ public class CheckController {
         String t = SensitiveWordUtils.sensitiveWordFiltering(text, false, false);
         return ResponseData.buildResponseToStandard(t);
     }
+
+    @ApiOperation("校验token")
+    @GetMapping("/verifyToken")
+    @Validation
+    public void verifyToken() {
+
+    }
+
 }
